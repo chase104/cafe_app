3d708a2184a5407110e1eed8d93e40a8548e3ca9
Chase Van Halen <cvanhalen@perscholas.org>
2023-02-14 14:11:02 -0600

    removed .env file

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const logger = require('morgan');
 // cross origin access 
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const User = require('./models/user.js')
+const User = require('./models/user.js');
+
+const passport = require('passport');
+const session = require('express-session');
+const initializePassport = require('./config/passport-config.js')
+
 require('dotenv').config();
 require('./config/database.js');
 
@@ -20,6 +25,26 @@ app.use(logger('dev'))
 
 //parse stringified objects (JSON)
 app.use(express.json())
+
+
+initializePassport(
+    passport,
+    // passport tells us that they want a function that will return the correct user given an email
+    async email => {
+        let user = User.findOne({email: email})
+        return user;
+    },
+    async id => {
+        let user = User.findById(id);
+        return user;
+    },
+);
+
+app.use(session({
+    secure: true,
+    secret: 
+}))
+
 
 // server build folder
 app.use(express.static(path.join(__dirname, 'build')));
@@ -46,6 +71,7 @@ app.post('/users/signup',async (req, res) => {
 
 app.put('/users/login', async (req, res) => {
     console.log(req.body);
+    // passport authentication
 })
 
 // catch all route
